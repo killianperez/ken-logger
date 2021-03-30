1d5b6b99df56f8105466245c6d9a0332e6fe0d8b
Killian P <kikihzhz@gmail.com>
2021-03-30 23:12:33 +0200

    Create example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+import logger
+
+logger.log(0, "DARK GRAY   -- NO PREFIX")
+logger.log(1, "DARK BLUE   -- NO PREFIX")
+logger.log(2, "DARK GREEN  -- SUCCESS/OK")
+# logger.log(3, "DOESN'T EXIST")
+logger.log(4, "RED         -- FAILED")
+logger.log(5, "PURPLE      -- DEBUG/TEMP")
+logger.log(6, "YELLOW      -- WARN")
+logger.log(7, "WHITE       -- NO PREFIX")
+# logger.log(8, "DOESN'T EXIST")
+logger.log(9, "LIGHT BLUE  -- INFO")
